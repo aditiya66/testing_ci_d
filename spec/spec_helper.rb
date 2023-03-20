f5198d577fee1b37be3d95264df51844688de213
Aditiya Nugraha <aditiyadev66@gmail.com>
2023-03-20 15:57:01 +0700

    remove selenium

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,27 +18,27 @@ SimpleCov.start do
   add_group 'Policies', '/app/policies'
   add_group 'Uploaders', '/app/uploaders'
 end
-Capybara.register_driver :headless_chrome do |app|
-  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
-      chromeOptions: { args: %w[headless disable-gpu] },
-      'goog:loggingPrefs': {
-          browser: 'ALL'
-      }
-    )
+# Capybara.register_driver :headless_chrome do |app|
+#   capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+#       chromeOptions: { args: %w[headless disable-gpu] },
+#       'goog:loggingPrefs': {
+#           browser: 'ALL'
+#       }
+#     )
 
-  options = ::Selenium::WebDriver::Chrome::Options.new(args: %w[headless disable-gpu])
+#   options = ::Selenium::WebDriver::Chrome::Options.new(args: %w[headless disable-gpu])
 
-  # options.add_argument('headless')
-  # options.add_argument('--no-sandbox')
-  # options.add_argument('--window-size=1400,1400')
+#   # options.add_argument('headless')
+#   # options.add_argument('--no-sandbox')
+#   # options.add_argument('--window-size=1400,1400')
 
-  Capybara::Selenium::Driver.new(
-      app,
-      browser: :chrome,
-      desired_capabilities: capabilities,
-      options: options
-    )
-end
+#   Capybara::Selenium::Driver.new(
+#       app,
+#       browser: :chrome,
+#       desired_capabilities: capabilities,
+#       options: options
+#     )
+# end
 
 Capybara.default_driver = :apparition
 Capybara.javascript_driver = :apparition
